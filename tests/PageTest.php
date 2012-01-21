7e531187f2a4a5a3c46de4f584d9505e473d1649
Walter Dal Mut <walter.dalmut@gmail.com>
2012-01-21 18:15:55 +0100

    Base tests

---
+++ tests/PageTest.php
@@ -0,0 +1,27 @@
+<?php 
+
+class PageTest extends PHPUnit_Framework_TestCase
+{
+    public function testValidBase()
+    {
+        $html = file_get_contents(__DIR__ . '/stuffs/little.html');
+        
+        $page = new Walk\Site\Page($html);
+        $id = $page->parse()->getId();
+        
+        $this->assertEquals('post_1088', $id);
+    }
+    
+    public function testRealPage()
+    {
+        $html = file_get_contents(__DIR__ . '/stuffs/single-content.html');
+    
+        $page = new Walk\Site\Page($html);
+
+        $id = $page->parse()->getId();
+        $this->assertEquals('post_1088', $id);
+        
+        $title = $page->getTitle();
+        $this->assertEquals('Zend\Di impressioni - ZF2', $title);
+    }
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,23 @@
+<?php 
+
+set_include_path(
+    implode(
+        PATH_SEPARATOR,
+        array(
+            __DIR__,
+            realpath(__DIR__ . '/../src'),
+            realpath(__DIR__ . '/../vendor/zf2/library'),
+            realpath(__DIR__ . '/../vendor/php-color-console/src'),
+            realpath(__DIR__ . '/../vendor/phly/Phly_PubSub/library'),
+            get_include_path()
+        )
+    )
+);
+
+require_once 'Zend/Loader/StandardAutoloader.php';
+$loader = new \Zend\Loader\StandardAutoloader();
+$loader->registerNamespace("Zend", __DIR__ . '/../vendor/zf2/library/Zend');
+$loader->registerNamespace("Wally", __DIR__ . '/../vendor/php-color-console/src/Wally');
+$loader->registerNamespace("phly", __DIR__ . '/../vendor/phly/Phly_PubSub/library/phly');
+$loader->registerNamespace("Walk", __DIR__ . '/../src/Walk');
+$loader->register();
