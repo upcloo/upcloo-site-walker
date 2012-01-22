07309bc2643847723bf92e9bed8d61fe5503167f
Walter Dal Mut <walter.dalmut@gmail.com>
2012-01-22 08:06:11 +0100

    change copy info

--- src/upcloo-walk.php
+++ src/upcloo-walk.php
@@ -1,6 +1,6 @@
 <?php
 /** 
- * Copyright (C) 2011 Walter Dal Mut, Gabriele Mittica
+ * Copyright (C) 2012 Corley S.R.L.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
